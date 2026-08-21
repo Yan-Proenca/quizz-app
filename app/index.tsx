16f0904e47004243674004ae11317f873d8a1128
Yan-Proenca <yanproenca.senai@gmail.com>
2026-08-21 12:13:09 -0300

    Teste

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
 import QuizScreen from "../components/QuizScreen";
+import ResultScreen from "../components/ResultScreen";
+import questions from "../questions.json";
 
-const HomePage = () => {
-  return <QuizScreen />;
-};
+export default function HomePage() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [isOptionsDisabled, setIsOptionsDisabled] = useState(false);
+  const [score, setScore] = useState(0);
+  const [isQuizFinished, setIsQuizFinished] = useState(false);
 
-export default HomePage;
+  const currentQuestion = questions[currentQuestionIndex];
+
+  const handleOptionPress = (option: string) => {
+    if (option === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+    }
+    setSelectedOption(option);
+    setIsOptionsDisabled(true);
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelectedOption(null);
+      setIsOptionsDisabled(false);
+    } else {
+      setIsQuizFinished(true);
+    }
+  };
+
+  const handlePlayAgain = () => {
+    setScore(0);
+    setCurrentQuestionIndex(0);
+    setSelectedOption(null);
+    setIsOptionsDisabled(false);
+    setIsQuizFinished(false);
+  };
+
+  return isQuizFinished ? (
+    <ResultScreen
+      score={score}
+      totalQuestions={questions.length}
+      onPlayAgain={handlePlayAgain}
+    />
+  ) : (
+    <QuizScreen
+      currentQuestion={currentQuestion}
+      selectedOption={selectedOption}
+      isOptionsDisabled={isOptionsDisabled}
+      onOptionPress={handleOptionPress}
+      onNextQuestion={handleNextQuestion}
+    />
+  );
+}
